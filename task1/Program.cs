07f9249bd720d5c087d3c7e9bbb0177bccd7a916
Anastasia <nastya-shishkina-1999@mail.ru>
2022-10-18 11:11:41 +0300

    task 1

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23 - 
+//делится и на 7 и на 23 без остатка
+
+Console.Clear();
+Console.Write("Введите число, которое хотите проверить: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"Число {number} кратно и 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {number} не кратно и 7 и 23");
+}
