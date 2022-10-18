941f4fffd2d908366059a0221415db75172a6d5d
Anastasia <nastya-shishkina-1999@mail.ru>
2022-10-18 11:36:10 +0300

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+Console.Clear();
+Console.Write("Введите число, которое хотите проверить: ");
+int number = int.Parse(Console.ReadLine());
+int digit1 = number % 10;
+int digit2 = (number / 10) % 10;
+int digit4 = (number /1000) % 10;
+int digit5 = (number /10000) % 10;
+if (digit1 == digit5 && digit2 == digit4)
+{
+    Console.WriteLine($"Число {number} палиндромное");
+}
+else 
+{
+     Console.WriteLine($"Число {number} не палиндромное");
+}
